07a5ef4e85559a564967d508868b155c0ccc43bd
Jitendra Sharma <jitendraps2001@gmail.com>
2024-04-15 14:04:10 +0530

    animated text

---
+++ src/Components/TextMain.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TextMain = () => {
+  return (
+    <div className='text-wrapper'>
+      <span className="text">K h y a t i</span>
+    </div>
+  )
+}
+
+export default TextMain
